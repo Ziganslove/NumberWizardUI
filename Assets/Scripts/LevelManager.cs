342b1d29d9b23cfcfd0ffd920111510cbaa06066
Anton Kurnikov <becomehuman97@mail.ru>
2021-12-06 19:15:39 +0300

    added a project

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Загрузка уровня " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Выход");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class NumberWizard : MonoBehaviour
+{
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+    public int maxGuessesAllowed = 10;
+
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1001;
+        min = 1;
+        NextGuess();
+    }
+
+    public void GuessHiger()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max);
+        maxGuessesAllowed--;
+        if (maxGuessesAllowed <= 0)     { SceneManager.LoadScene("Win"); }
+        else text.text = guess.ToString();
+    }
+}
